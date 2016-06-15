d366dc0710be05f37c768427bbe9de33bd52646c
WoodMath <jeffwood314@gmail.com>
2016-06-14 23:56:33 -0500

    Added [Park2003] implementation

--- StereoPoints/fnPark.m
+++ StereoPoints/fnPark.m
@@ -8,7 +8,7 @@ function [  ] = fnPark( img_one, img_two, vRC_blocks )
         vRC_size = size(img_one);
     end
     
-    vRC_block_size = uint8([1,1]);
+    vRC_block_size = uint64([1,1]);
     if(mod(vRC_size(1), vRC_blocks(1)) ~= 0)
         error(' Image Height not evenly dvisible by bumber of block rows ');
     else
@@ -19,99 +19,153 @@ function [  ] = fnPark( img_one, img_two, vRC_blocks )
     else
         vRC_block_size(2) = round(vRC_size(2) / vRC_blocks(2));
     end
+
+    %% Make grayscale if not already
+    if(size(img_one,3) ~= 1)
+        img_one_bw = rgb2gray(img_one);
+    else
+        img_one_bw = img_one;
+    end
+    if(size(img_two,3) ~= 1)
+        img_two_bw = rgb2gray(img_two);
+    else
+        img_two_bw = img_two;
+    end
     
-        
-    img_one_bw = rgb2gray(img_one);
-    img_two_bw = rgb2gray(img_two);
+    img_one_bw_grad = imgradient(img_one_bw);
+    img_two_bw_grad = imgradient(img_two_bw);
+
+%     %% Using Sobel method as described in articles
+%     filter_sobel = fspecial('sobel');
+%     im_vert = imfilter(img_one_bw,filter_sobel);
+%     im_horz = imfilter(img_one_bw,filter_sobel');
+%     % img_one_bw_grad = fnNormalize(double(im_vert)+double(im_horz));
+%     img_one_bw_grad = double(im_vert)+double(im_horz);
+%     im_vert = imfilter(img_two_bw,filter_sobel);
+%     im_horz = imfilter(img_two_bw,filter_sobel');
+%     % img_two_bw_grad = fnNormalize(double(im_vert)+double(im_horz));
+%     img_two_bw_grad = double(im_vert)+double(im_horz);
+%     clear('im_vert','im_horz');
     
     
-    img1_mask = zeros([vRC_size(1), vRC_size(2), vRC_blocks(1), vRC_blocks(2)],'uint8');
+    struct_mask = struct('Rows',[],'Columns',[],'Offset',[],'ImageOne',[],'ImageTwo',[]);
+
+    img_grad_one = zeros([vRC_block_size, vRC_blocks]);
+    img_grad_two = zeros([vRC_block_size, vRC_blocks]);
     
     v_rows = ones(1, vRC_block_size(1),'uint64');
     v_cols = ones(1, vRC_block_size(2),'uint64');
+    
     for i_inc = uint64(1:vRC_blocks(1))
         for j_inc = uint64(1:vRC_blocks(2))
             v_rows(:) = uint64(1:vRC_block_size(1)) + ((i_inc-1)*uint64(ones(1,vRC_block_size(1))*double(vRC_block_size(1))));
             v_cols(:) = uint64(1:vRC_block_size(2)) + ((j_inc-1)*uint64(ones(1,vRC_block_size(2))*double(vRC_block_size(2))));
-            img1_mask(v_rows,v_cols,i_inc,j_inc) = true;
+            struct_mask(i_inc, j_inc).Rows = v_rows;
+            struct_mask(i_inc, j_inc).Columns = v_cols;
+            struct_mask(i_inc, j_inc).Offset = [v_rows(1)-1, v_cols(1)-1]; 
+
+            img_temp_one = img_one_bw_grad(v_rows,v_cols);
+            img_temp_two = img_two_bw_grad(v_rows,v_cols);
+            
+            struct_one = struct('ImageData',img_one_bw(v_rows,v_cols),'GradientData',img_temp_one);
+            struct_two = struct('ImageData',img_two_bw(v_rows,v_cols),'GradientData',img_temp_two);
+            
+            struct_mask(i_inc, j_inc).ImageOne = struct_one;
+            struct_mask(i_inc, j_inc).ImageTwo = struct_two;
+            
+            img_grad_one(:,:,i_inc,j_inc) = img_temp_one;
+            img_grad_two(:,:,i_inc,j_inc) = img_temp_two;
+            
         end
     end
     
-    clear('v_rows')
-    clear('v_cols')
+    clear('img_temp_one','img_temp_two');
+    clear('v_rows','v_cols');
+    clear('struct_one','struct_two');
     
-%     img3_mask = cat(5, img1_mask, img1_mask, img1_mask);
-%     img3_mask = permute(img3_mask, [1, 2, 5, 3, 4]);
 
-%     img_one_lab = rgb2lab(img_one);
-%     img_two_lab = rgb2lab(img_two);
     
 
 
-
+    %% Extract points containing maximum gradient
+    f_thresh = 500;
+    img_one_max_grad = img_grad_one;
+    img_one_max_grad = max(max(img_one_max_grad,[],1),[],2);
+    img_one_max_grad = repmat(img_one_max_grad, [vRC_block_size, 1, 1]);
+    img_one_places = uint8((img_grad_one==img_one_max_grad).*(img_grad_one>=f_thresh));
+    img_one_count = sum(sum(uint8(img_one_places),1),2);
+    img_one_count = permute(img_one_count, [3,4,1,2]);
     
-%     filter_sobel = fspecial('sobel');
-%     im_vert = imfilter(img_one_bw,filter_sobel);
-%     im_horz = imfilter(img_one_bw,filter_sobel');
-%     im_comb = fnNormalize((double(im_vert).^2+double(im_horz).^2).^0.5);
-% 
-%     figure(5);
-%     imshow(im_vert);
-%     figure(6);
-%     imshow(im_horz);
-%     figure(7);
-%     imshow(im_comb);
-%     
+    img_two_max_grad = img_grad_two;
+    img_two_max_grad = max(max(img_two_max_grad,[],1),[],2);
+    img_two_max_grad = repmat(img_two_max_grad, [vRC_block_size, 1, 1]);
+    img_two_places = uint8((img_grad_two==img_two_max_grad).*(img_grad_two>=f_thresh));
+    img_two_count = sum(sum(uint8(img_two_places),1),2);
+    img_two_count = permute(img_two_count, [3,4,1,2]);
 
-    [img_one_bw_grad_mag, img_one_bw_grad_dir] = imgradient(img_one_bw);
-    [img_two_bw_grad_mag, img_two_bw_grad_dir] = imgradient(img_two_bw);
-    
-    [img_one_grad_part] = repmat(img_one_bw_grad_mag, [1, 1, size(img1_mask,3), size(img1_mask,4)]).*double(img1_mask);
-    [img_two_grad_part] = repmat(img_two_bw_grad_mag, [1, 1, size(img1_mask,3), size(img1_mask,4)]).*double(img1_mask);
-    
-    clear('img_one_bw_grad_dir');
-    clear('img_two_bw_grad_dir');
-    
-    img_one_max_grad = max(max(img_one_grad_part,[],1),[],2);
-    img_one_max_grad = repmat(img_one_max_grad, [vRC_size(1), vRC_size(2), 1, 1]);
-    img_one_places = (img_one_grad_part==img_one_max_grad);
-
-    img_two_max_grad = max(max(img_two_grad_part,[],1),[],2);
-    img_two_max_grad = repmat(img_two_max_grad, [vRC_size(1), vRC_size(2), 1, 1]);
-    img_two_places = (img_two_grad_part==img_one_max_grad);
-
-    clear('img_one_max_grad');
-    clear('img_two_max_grad');
+    clear('img_one_max_grad','img_two_max_grad');
     
     %% Get RC positions of max places
-    img_one_place_RC = reshape(double(img_one_places), [vRC_size(1)*vRC_size(2),1,vRC_blocks]);
-    img_two_place_RC = reshape(double(img_two_places), [vRC_size(1)*vRC_size(2),1,vRC_blocks]);
+    img_one_place_RC = reshape(double(img_one_places), [vRC_block_size(1)*vRC_block_size(2),1,vRC_blocks]);
+    img_two_place_RC = reshape(double(img_two_places), [vRC_block_size(1)*vRC_block_size(2),1,vRC_blocks]);
     
     img_one_place_RC = permute(img_one_place_RC, [1,3,4,2]);
     img_two_place_RC = permute(img_two_place_RC, [1,3,4,2]);
     
-    mat_idx = double([1:vRC_size(1)*vRC_size(2)])';
+    mat_idx = double([1:(vRC_block_size(1)*vRC_block_size(2))])';
     mat_idx = repmat(mat_idx, [1, vRC_blocks]);
     
     img_one_place_RC = sum(img_one_place_RC.*mat_idx,1);
     img_two_place_RC = sum(img_two_place_RC.*mat_idx,1);
     
-    img_one_place_RC = permute(img_one_place_RC,[2,3,1]);
-    img_two_place_RC = permute(img_two_place_RC,[2,3,1]);
+    img_one_place_RC = uint64(permute(img_one_place_RC,[2,3,1]));
+    img_two_place_RC = uint64(permute(img_two_place_RC,[2,3,1]));
+
+    %% Only include that points where both images are greater than threshold
+    b_img_one = (img_one_place_RC>0);
+    b_img_two = (img_two_place_RC>0);
+    b_include = ((img_one_place_RC>0) & (img_two_place_RC>0));
+    b_include = reshape(b_include, [vRC_blocks(1)*vRC_blocks(2),1]);
+    clear('b_img_one','b_img_two');
     
-    img_one_place_row = idivide(img_one_place_RC - 1, vRC_size(1))+1; 
-    img_two_place_row = idivide(img_one_place_RC - 1, vRC_size(1))+1; 
+    img_one_place_row = mod(img_one_place_RC - 1, vRC_block_size(1))+1; 
+    img_two_place_row = mod(img_two_place_RC - 1, vRC_block_size(1))+1; 
+
+    img_one_place_col = idivide(img_one_place_RC - 1, vRC_block_size(1))+1; 
+    img_two_place_col = idivide(img_two_place_RC - 1, vRC_block_size(1))+1; 
     
+    %% Add back in offsets
+    v_offsets = reshape([struct_mask.Offset],[2,vRC_blocks(1)*vRC_blocks(2)])';
+    img_one_place_row = reshape(img_one_place_row, [vRC_blocks(1)*vRC_blocks(2),1])+v_offsets(:,1);
+    img_two_place_row = reshape(img_two_place_row, [vRC_blocks(1)*vRC_blocks(2),1])+v_offsets(:,1);
+
+    img_one_place_col = reshape(img_one_place_col, [vRC_blocks(1)*vRC_blocks(2),1])+v_offsets(:,2);
+    img_two_place_col = reshape(img_two_place_col, [vRC_blocks(1)*vRC_blocks(2),1])+v_offsets(:,2);
+    
+    %% Filter out points not above threshold
+    img_one_place_row = img_one_place_row(b_include,:);
+    img_one_place_col = img_one_place_col(b_include,:);
+    
+    img_two_place_row = img_two_place_row(b_include,:);
+    img_two_place_col = img_two_place_col(b_include,:);
     
     figure(1);
     imshow(img_one_bw);
-%     figure(2);
-%     imshow(img_two_bw);
+    hold on;
+    plot(img_one_place_col, img_one_place_row, '.r');
+    figure(2);
+    imshow(img_two_bw);
+    hold on;
+    plot(img_two_place_col, img_two_place_row, '.r');
 
     
     figure(3);
-    imshow(fnNormalize(img_one_bw_grad_mag));
-%     figure(4);
-%     imshow(fnNormalize(img_two_bw_grad_mag));
+    imshow(fnNormalize(img_one_bw_grad));
+    hold on;
+    plot(img_one_place_col, img_one_place_row, '.r');
+    figure(4);
+    imshow(fnNormalize(img_two_bw_grad));
+    hold on;
+    plot(img_two_place_col, img_two_place_row, '.r');
 end
 
--- StereoPoints/test_park.m
+++ StereoPoints/test_park.m
@@ -1,7 +1,12 @@
 
 
     warning('off','images:initSize:adjustingMag');
-%     im_im0 = imread('./StereoImages/im0.png');
-%     im_im1 = imread('./StereoImages/im1.png');
+    im_im0 = imread('./StereoImages/im0.png');
+    im_im1 = imread('./StereoImages/im1.png');
     
-    fnPark(im_im0, im_im1, [16,12]);
+%     fnPark(im_im0, im_im1, [16,12]);
+
+    im_im0 = imresize(im_im0, 0.5);
+    im_im1 = imresize(im_im1, 0.5);
+    fnPark(im_im0, im_im1, [4,3]);
+    
