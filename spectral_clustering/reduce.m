bffa995d3d675592ddb6e563d7a18460442531ea
WoodMath <jeffwood314@gmail.com>
2016-08-12 14:44:41 -0500

    Updated code for overlapping sub areas

--- spectral_clustering/reduce.m
+++ spectral_clustering/reduce.m
@@ -1,7 +1,7 @@
 % clear all;
 % close all;
 
-if ~logical(exist('im_fl'))
+if ~logical(exist('im_fl','var'))
     disp(' Loading... ');
     im_fl = imread('MyNameIsFlorida.jpg');
 end
@@ -17,12 +17,12 @@ end
 i_square_size = 64;
 v2_partition_numbers = [v3_size(1)/i_square_size, v3_size(2)/i_square_size];
 
-v5_partitions = zeros([i_square_size, i_square_size, i_dim3, v2_partition_numbers(1), v2_partition_numbers(2)], 'uint8');
 
 %% Warning:
 % loops assume that image size is a perfect multiple of 'i_sqaure_size' and
 % 'v2_partition_numbers'
 
+v5_partitions = zeros([i_square_size, i_square_size, i_dim3, v2_partition_numbers(1), v2_partition_numbers(2)], 'uint8');
 % i_row_start = 1;
 % for i_inc = 1:v2_partition_numbers(1)
 %     v2_rows = uint64([i_row_start:(i_row_start+i_square_size-1)]);
@@ -48,7 +48,7 @@ v5_partitions = zeros([i_square_size, i_square_size, i_dim3, v2_partition_number
 i_reduce = 16;
 v3_size_reduce = v3_size./[i_reduce,i_reduce,1];
 
-if ~logical(exist('im_fl_re'))
+if ~logical(exist('im_fl_re','var'))
     disp(' Reducing... ');
     im_fl_reduce = imresize(im_fl, 1/i_reduce);
 end
@@ -58,3 +58,24 @@ end
 %% Warning:
 % again, loops assume that image size is a perfect multiple of 'i_sqaure_size' and
 % 'v2_partition_numbers'
+
+v5_overlap = zeros([i_square_size, i_square_size, i_dim3, v2_partition_numbers(1)-1, v2_partition_numbers(2)-1], 'uint8');
+i_row_start = uint64(i_square_size/2+1);
+for i_inc = 1:(v2_partition_numbers(1)-1)
+    v2_rows = uint64([i_row_start:(i_row_start+i_square_size-1)]);
+
+    i_col_start = uint64(i_square_size/2+1);
+    for j_inc = 1:(v2_partition_numbers(2)-1)
+        v2_cols = uint64([i_col_start:(i_col_start+i_square_size-1)]);
+        
+        
+        %% Where the magic happens
+        v5_overlap(:,:,:,i_inc,j_inc) = im_fl(v2_rows, v2_cols, :);
+        
+
+        
+        
+        i_col_start = i_col_start + i_square_size;
+    end
+    i_row_start = i_row_start + i_square_size;
+end
